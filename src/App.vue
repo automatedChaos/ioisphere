c29fb2334f41acac5cbc7f7b60f6ccdc1edb35c9
Alcwyn <alcwynparker@hotmail.co.uk>
2018-03-13 00:37:45 +0000

    the split view panels are working

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Split class="parent-split" :direction="'vertical'">
+      <SplitArea>
+          Visual Editor
+      </SplitArea>
+      <SplitArea>
+        <Split style="height: 500px;">
+          <SplitArea :size="25">
+              <router-view/>
+          </SplitArea>
+          <SplitArea :size="75">
+              Sphere
+          </SplitArea>
+        </Split>
+      </SplitArea>
+  </Split>
+
+  <!--  -->
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  mounted: function () {
+    var parentPanel = document.getElementsByClassName('parent-split')
+    parentPanel[0].style.height = `${window.innerHeight}px`;
+  }
 }
 </script>
 
@@ -20,4 +39,15 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+html, body {
+    height: 100%;
+  }
+body {
+  padding: 8px;
+  background-color: #F6F6F6;
+  box-sizing: border-box;
+}
+
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
+/**
+ * @Author: alcwynparker
+ * @Date:   2018-03-12T22:22:05+00:00
+ * @Last modified by:   alcwynparker
+ * @Last modified time: 2018-03-12T23:56:59+00:00
+ */
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueSplit from 'vue-split-panel'
+
+import './assets/sass/ioi.scss'
 
 Vue.config.productionTip = false
+Vue.use(VueSplit)
 
 /* eslint-disable no-new */
 new Vue({
